f015fde22c761420cbc90b12ddf4a1dc45246138
chunboo <chunboo.liu@gmail.com>
2024-02-13 15:16:09 +0800

    0213 added the pointer codes

---
+++ pointer.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    int *ptr=(&a+1);
+    printf("%d,%d\n",*(a+1),*(ptr-1));  //output 2,5
+    printf("%p \n",a);  //0xFE00
+    printf("%p \n",&a); //0xFE00
+    printf("%p \n",a+1); //0xFE04
+    printf("%p \n",&a+1); //0xFE14
+}
