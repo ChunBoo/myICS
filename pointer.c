35f76817517918f37a773c70a4b2f381ea31fdbf
chunboo <chunboo.liu@gmail.com>
2024-02-13 15:17:55 +0800

    0213-added some comments

--- pointer.c
+++ pointer.c
@@ -5,7 +5,7 @@
 int main()
 {
     int a[5]={1,2,3,4,5};
-    int *ptr=(&a+1);
+    int *ptr=(&a+1);  //ptr-a== sizeof(int)*5
     printf("%d,%d\n",*(a+1),*(ptr-1));  //output 2,5
     printf("%p \n",a);  //0xFE00
     printf("%p \n",&a); //0xFE00
