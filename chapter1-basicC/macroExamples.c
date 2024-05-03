746639689c02bbedbf7daad1a36fa5cc04acc583
chunboo <chunboo.liu@gmail.com>
2024-05-03 12:13:19 +0800

    0503-updated the ## marco

--- chapter1-basicC/macroExamples.c
+++ chapter1-basicC/macroExamples.c
@@ -3,17 +3,20 @@
 #define ARCH "x86-64"
 
 
-#define A "aaaaaaaaaa"
-#define TEN(A) A A A A A A A A A A
-#define B TEN(A)
-#define C TEN(B)
-#define D TEN(C)
-#define E TEN(D)
-#define G TEN(E)
+// #define A "aaaaaaaaaa"
+// #define TEN(A) A A A A A A A A A A
+// #define B TEN(A)
+// #define C TEN(B)
+// #define D TEN(C)
+// #define E TEN(D)
+// #define G TEN(E)
 
+
+
+#define A sys ## tem   
 void foo(){
     #if a==b
-    printf("yes\n");
+    printf("yes\n");  //output yes
     #else
     printf("No");
     #endif
@@ -22,5 +25,6 @@ int main()
 {
     // printf("The arch is " ARCH "\n");
     // foo();
-    puts(E);
+    // puts(E);
+    A("echo hello");  //system("echo hello");
 }
